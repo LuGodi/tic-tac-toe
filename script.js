285f17465095da345e76dd312151beb2e779c6fb
Lulu <112640408+HopeICanChangeMyUsername@users.noreply.github.com>
2024-02-20 11:41:10 -0300

    add gameController object to control flow of the game and player factory function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-gameboard = (function () {
-  board = [];
+const gameboard = (function () {
+  const board = [];
   const resetGameboard = () => {
     for (let row = 0; row < 3; row++) {
       board.push([]);
@@ -7,9 +7,52 @@ gameboard = (function () {
         board[row].push(null);
       }
     }
+    return board;
   };
   const getGameboard = () => board;
-  const getCell = (row, column) => board[row][column];
+  const getGameboardCell = (row, column) => board[row][column];
+  const setCellValue = (row, column, mark) => (board[row][column] = mark);
+  return { getGameboard, resetGameboard, getGameboardCell, setCellValue };
+})();
 
-  return { getGameboard, resetGameboard, getCell };
+function CreatePlayer(name, mark) {
+  let score = 0;
+
+  const getPlayerScore = () => score;
+  //should this be on the scoreboard object or here?
+  const incrementPlayerScore = () => ++score;
+  const resetPlayerScore = () => (score = 0);
+  const getPlayerMark = () => mark;
+  const getPlayerName = () => name;
+
+  return {
+    getPlayerScore,
+    incrementPlayerScore,
+    resetPlayerScore,
+    getPlayerMark,
+    getPlayerName,
+  };
+}
+
+const gameController = (function () {
+  let playerTurn;
+  const startNewGame = function () {
+    gameboard.resetGameboard();
+    player1 = CreatePlayer("player1", "X");
+    player2 = CreatePlayer("player2", "O");
+    playerTurn = player1;
+
+    //so vai existir nessa funcao ? acho que existe fora pq n ta com let
+  };
+  const changePlayer = () =>
+    (playerTurn = playerTurn === player1 ? player2 : player1);
+  const getCurrentPlayer = () => playerTurn;
+  const playMove = function (row, column) {
+    let currentPlayer = playerTurn;
+    gameboard.setCellValue(row, column, currentPlayer.getPlayerMark());
+    gameboard.getGameboard(); //change to render in the future
+    changePlayer();
+  };
+
+  return { startNewGame, getCurrentPlayer, playMove };
 })();
